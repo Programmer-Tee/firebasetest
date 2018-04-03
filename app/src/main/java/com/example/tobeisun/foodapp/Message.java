89e8e6b455abc6f63577ffe31bf997aa76d32ccb
t.obeisun <obeisuntabithaa@gmail.com>
2018-04-03 15:59:31 +0100

    modified

--- app/src/main/java/com/example/tobeisun/foodapp/Message.java
+++ app/src/main/java/com/example/tobeisun/foodapp/Message.java
@@ -1,16 +1,23 @@
 package com.example.tobeisun.foodapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -32,7 +39,19 @@ public class Message extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
 
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Start sign in/sign up activity
+            startActivity(new Intent(Message.this, Login.class));
+        } else {
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+
+
+            // Load chat room contents
+            display();
+        }
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view) {
                 input = (EditText) findViewById(R.id.editTextInput);
@@ -51,10 +70,15 @@ public class Message extends AppCompatActivity {
                 // Clear the input
                 input.setText("");
 
+
+
             }
+
+
         });
         display();
     }
+
 
     public void display() {
         adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
@@ -78,5 +102,32 @@ public class Message extends AppCompatActivity {
         };
 
         listView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_sign_out) {
+            AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(Message.this,
+                                    "You have been signed out.",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+
+                            // Close activity
+                            finish();
+                        }
+                    });
+        }
+        return true;
     }
 }
